a6acdc80b2587a90f9a7ed0cd6f5909e4f640dd4
super lap 2 <mickey27esc@gmail.com>
2024-11-28 14:30:41 -0600

    final version chat - no db connected

--- chatbot.py
+++ chatbot.py
@@ -2,25 +2,49 @@
 import streamlit as st
 import google.generativeai as genai
 
-st.title("🤖📂 RAG App")
+st.title("🤖📂 Gemini based RAG App")
 
 API_KEY = st.sidebar.text_input("GEMINI API Key", type="password")
 
+# Set Gemini API key from Streamlit secrets
+genai.configure(api_key=API_KEY)
 
-def generate_response(input_text):
-    genai.configure(api_key=API_KEY)
-    model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content(input_text)
-    st.info(response)
+# Initialize model
+if "model" not in st.session_state:
+    st.session_state.model = genai.GenerativeModel("gemini-1.5-flash")
 
+# Initialize chat history
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
 
-with st.form("my_form"):
-    text = st.text_area(
-        "Enter text:",
-        "What are the three key pieces of advice for learning how to code?",
+# Initialize chat
+if "chat" not in st.session_state:
+    st.session_state.chat = st.session_state.model.start_chat(
+        history=st.session_state.chat_history,
     )
-    submitted = st.form_submit_button("Submit")
-    if not API_KEY.endswith("-pk"):
-        st.warning("Please enter your GEMINI API key!", icon="⚠")
-    if submitted and API_KEY.endswith("-pk"):
-        generate_response(text)
+    
+
+# Display chat messages from history on app rerun
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("Escribe aquí..."):
+    # Add user message to chat history
+    st.session_state.chat_history.append(
+        dict(
+            role='user',
+            content=prompt,
+        )
+    )
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):   
+        # Call Gemini API to generate the assistant's response based on chat
+        response = st.session_state.chat.send_message(prompt)
+        st.markdown(response.text)  # Display the response
+    # Add AI message to chat history
+    st.session_state.chat_history.append({"role": "assistant", "content": response.text})
