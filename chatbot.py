f1317b455602418d01954d23bcd804f7075bbbc5
Miguel <mickey27esc@gmail.com>
2024-12-01 20:32:02 -0600

    new code pdf

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,11 @@
-#Restarting proyect
+import os
+import chromadb
+from chromadb.config import DEFAULT_TENANT, DEFAULT_DATABASE, Settings
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+
+import google.generativeai as genai
+
+def get_relevant_passage(query, db_collection):
+    passage = db_collection.query(query_texts=[query], n_results=1)['documents'][0][0]
+    return passage
